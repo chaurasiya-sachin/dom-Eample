43d0861b0f1c4871b24dcb8628d730b72d7f5f45
Rahul <chaurasiyasachin434@gmail.com>
2024-10-23 22:39:00 +0530

    learing dom with the help of div

---
+++ script.js
@@ -0,0 +1,10 @@
+let box = document.getElementById("box-div");
+
+box.style.backgroundColor = "Yellow";
+
+box.style.margin= "20px";
+box.style.padding="10px";
+box.style.fontSize="18px";
+box.style.fontWeight="bold";
+box.style.height="200px";
+box.style.width="300px";
